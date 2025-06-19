b635891964e49c3267e6b1d970c7c84d8111897e
nonbinary00 <agarkovaalysh12@gmail.com>
2025-06-19 22:03:29 +0200

    started adaptive
    Made an adaptive for a couple of sections, connected images for the retina screen. Connected a swipe with pagination

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    320: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 },
+  },
+});
